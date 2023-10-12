b31b742770707143a10bad3ab328d8d1851d87ba
neiswin <nikolaswind@gmail.com>
2023-10-12 11:59:57 +0300

    add slimselect

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,18 @@ import * as bootstrap from "bootstrap"
 import "trix"
 import "@rails/actiontext"
 
+import SlimSelect from 'slim-select'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tagSelect = new SlimSelect({
+    select: '#tag_ids',
+    settings: {
+      placeholderText: 'Выберите теги',
+    }, // Настройте placeholder по вашему усмотрению
+    allowDeselect: true, // Настройки SlimSelect по вашему выбору
+    // Другие настройки SlimSelect...
+  });
+});
 
 
 
