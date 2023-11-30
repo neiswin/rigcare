f4de7bcc6284044ad591246dc8320cb3b6283f57
deploy <vpp@beloil.by>
2023-11-30 13:55:16 +0300

    deploy

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module RigcareBn
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -20,5 +22,14 @@ module RigcareBn
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins 'https://psopc.beloil.by'
+          resource '*',
+                    headers: :any,
+                    methods: [:get, :post, :put, :patch, :delete, :options, :head],
+                    credentials: true
+        end
+    end
   end
 end
