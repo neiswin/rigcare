c5fde2c5bfeaa3dbffda6dfa7b0c681bffadf454
neiswin <nikolaswind@gmail.com>
2023-12-05 11:44:20 +0300

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,9 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.18.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
 set :application, "myapp"
 set :repo_url, "git@github.com:neiswin/rigcare.git"
+set :branch, 'main'
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
