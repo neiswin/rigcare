3176cd27eaeeeff8f53cbf5476308b21524cc7ca
deploy <vpp@beloil.by>
2025-05-27 17:23:14 +0300

    start butoforia

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,29 @@
 class PagesController < ApplicationController
+  # главная страница
+  def home
+  end
+
+  # статические заглушки
+  def mudpumps   # буровые насосы
+  end
+
+  def das        # СКПБ
+  end
+
+  def td         # Система верхнего привода
+  end
+
+  def drawwork   # лебёдка
+  end
+
+  # существующие:
   def contact
     @users = User.all.decorate
   end
 
-
   def index
     @faqs = Faq.last(5)
   end
-
 
   def test
     if params[:query].present?
--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,59 @@
 Rails.application.routes.draw do
-  get 'equipments/index'
-  get 'equipments/show'
-  get 'equipments/new'
-  get 'equipments/edit'
+  # get 'equipments/index'
+  # get 'equipments/show'
+  # get 'equipments/new'
+  # get 'equipments/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
  
-  resources :faqs do
-    collection do
-      post :search
-    end
-  end
-  devise_for :users
+  # resources :faqs do
+  #   collection do
+  #     post :search
+  #   end
+  # end
+  # devise_for :users
 
-  namespace :admin do
-    devise_for :users 
-    resources :users, only: %i[index create edit update destroy]
-  end
+  # namespace :admin do
+  #   devise_for :users 
+  #   resources :users, only: %i[index create edit update destroy]
+  # end
 
-  resources :users, only: %i[index show]
+  # resources :users, only: %i[index show]
 
-  get 'tags/search', to: 'tags#search'
+  # get 'tags/search', to: 'tags#search'
 
-  post 'search', to: 'search#index', as: 'search'
+  # post 'search', to: 'search#index', as: 'search'
   
-  post 'search/suggestions', to: 'search#suggestions', as: 'search_suggestions'
+  # post 'search/suggestions', to: 'search#suggestions', as: 'search_suggestions'
 
 
 
   
-  root "pages#index"
-  get 'pages/contact'
+  # root "pages#index"
+
   # get '/test', to: 'tests#index'
 
-  resources :tests
+  # resources :tests
   # get 'phonebook/', to: 'phonebook_contacts#index'
 
-  resources :equipments
+  # resources :equipments
 
 
   # resources :phonebook_contacts, only: %i[create destroy edit index new update]
+
+
+
+
+
+  get 'pages/contact'
+  # Главная страница
+  root 'pages#home'
+
+  # Статические страницы
+  get 'mudpumps',  to: 'pages#mudpumps',  as: :mudpumps   # Буровые насосы
+  get 'das',       to: 'pages#das',       as: :das        # СКПБ
+  get 'td',        to: 'pages#td',        as: :td         # Система верхнего привода
+  get 'drawwork',  to: 'pages#drawwork',  as: :drawwork  # Лебёдка
+
 end
